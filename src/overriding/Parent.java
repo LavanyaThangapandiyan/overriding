fdc2e47b81fc49c754ce6e2ef56459dff3825ff0
valid <valid@admin>
2023-04-11 13:15:33 +0530

    overriding

---
+++ src/overriding/Parent.java
@@ -0,0 +1,35 @@
+package overriding;
+
+public class Parent 
+{
+		   public void doYoga()
+		   {
+		      System.out.println("good for health");
+		   }
+		   public void newsreading()
+		   {
+			   System.out.println("Reading newspapers will improve your knowledge");
+		   }
+		}
+
+		class son extends Parent
+		{
+		   @Override
+		   public void doYoga() 
+		   {
+		      System.out.println("Yoga is good for Health");
+		   }
+		}
+
+		class Main 
+		{
+		   public static void main(String[] args) 
+		   {
+		      son obj = new son();
+		      obj.newsreading();
+		      obj.doYoga();
+		   }
+		}
+
+
+
